8afa268b3f312f549c15318f09b4fb8bb8dbcc90
luetkemj <luetkemj@gmail.com>
2023-05-18 10:55:16 -0600

    update button

--- src/stories/Button.vue
+++ src/stories/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button type="button" :class="classes" @click="onClick" :style="style">{{ label }} LOOK MA! CHANGES!</button>
+  <button type="button" :class="classes" @click="onClick" :style="style">{{ label }}</button>
 </template>
 
 <script>
